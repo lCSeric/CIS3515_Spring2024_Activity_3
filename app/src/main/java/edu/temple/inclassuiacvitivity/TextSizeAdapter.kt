01345f7907e2c960ab9c7e390bf384b8bce4755f
Eric Lin <tur99299@temple.edu>
2024-02-08 12:19:59 -0500

    change the font size as itself.

--- app/src/main/java/edu/temple/inclassuiacvitivity/TextSizeAdapter.kt
+++ app/src/main/java/edu/temple/inclassuiacvitivity/TextSizeAdapter.kt
@@ -1,25 +1,43 @@
 package edu.temple.inclassuiacvitivity
 
+import android.content.Context
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.TextView
 
-class TextSizeAdapter : BaseAdapter() {
+class TextSizeAdapter( _context: Context, _numberArray: Array<Int>) : BaseAdapter() {
+
+    private val Context = _context
+    private val numberArray = _numberArray
 
     override fun getCount(): Int {
-        TODO("Not yet implemented")
+        return numberArray.size
     }
 
     override fun getItem(position: Int): Any {
-        TODO("Not yet implemented")
+        return numberArray[position]
     }
 
     override fun getItemId(position: Int): Long {
-        TODO("Not yet implemented")
+        return position.toLong()
     }
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        TODO("Not yet implemented")
+        val textView = TextView(Context)
+        textView.text = numberArray[position].toString()
+        //val numberSize = numberArray[position]
+        textView.textSize =  22f
+        textView.setPadding(5,10,0,10)
+        return textView
+
+    }
+    override fun getDropDownView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val textView = TextView(Context)
+        textView.text = numberArray[position].toString()
+        textView.textSize = numberArray[position].toFloat() // Set font size equal to the value of the number
+        textView.setPadding(5, 10, 0, 10)
+        return textView
     }
 
 }
